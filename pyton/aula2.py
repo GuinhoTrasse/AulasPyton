dfb53fff2f417cd38f0274515d2b4b7d5a3aed0d
GuinhoTrasse <guilherme.dias4@estudante.ifms.edu.br>
2026-03-31 20:12:07 -0400

    Add files via upload

---
+++ pyton/aula2.py
@@ -0,0 +1,37 @@
+# for i in range (10, 0, -1):
+#     print(i)
+
+
+# for i in range (0, 10):
+#     print(i)
+
+
+# for _ in range (0, 10):
+#     print("Olá")
+
+
+# n = 10
+# for i in range(n // 2):
+#     print(i)
+
+
+# for i in range(11):
+#     for y in range(11):
+#         print(f"{i} x {y} = {i * y}")
+
+
+# for i in range (10):
+#     print(i)
+# else:
+#     print("Fim")
+
+
+# for i in range(1, 11):
+#     print(i)
+#     if i + 1 == 4:
+#         break
+
+# for i in range (1, 11):
+#     if i == 5:
+#         continue
+#     print("Iteração", i)
---
+++ pyton/exercicios2.py
@@ -0,0 +1,27 @@
+# Faça com que um usuário entre com dez números. Após o usuário inserir o número apresente esse número pra ele.
+
+# for i in range (10):
+#     i = int(input("Digite um número\n"))
+#     print (i)
+
+
+# Faça um programa em Python que some os números pares no intervalo de 0 a 100
+
+# for i in range (101):
+#     if i % 2 == 0:
+#         print(i)
+
+
+# Faça um programa em Python que some os 30 primeiros números ímpares, e imprima o resultado.
+
+# y = 0
+# for i in range (1, 30, 2):
+#     y += i
+# print(y)
+
+
+# Faça um programa em Python que apresente a tabuada de um valor informado pelo usuário.
+
+# i =int(input("Digite um número para mostrar a tabuada do mesmo\n"))
+# for y in range (113):
+#         print(f"{i} x {y} = {y * i}")
